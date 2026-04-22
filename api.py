d27085240f1180662c1d995b7374cbe8df1a27bb
Rohit Maurya <rohitmaurya441865@gmail.com>
2026-04-22 12:02:39 +0530

    Fix: Corrected hallucinated LangChain import in API

--- api.py
+++ api.py
@@ -13,8 +13,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_classic.chains.combine_documents import create_stuff_documents_chain
-from langchain_classic.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 import chromadb
 
 load_dotenv()
